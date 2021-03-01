10357214cee8d128086c0e4ca9b63000d4b54a2e
unknown <rahzy24@gmail.com>
2021-03-01 21:28:31 +0100

    Updated work

--- Furnish.js
+++ Furnish.js
@@ -74,3 +74,26 @@ if (auto) {
 }
 
 
+
+
+///////////////////// mINIMIZED FOOTER JS //////////////////////////////////////
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
